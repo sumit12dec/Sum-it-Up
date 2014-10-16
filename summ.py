99eba15ff7e7e9ecb3321adcfd3c9e871c6eb395
root <sumit.raj12dec@gmail.com>
2014-10-16 22:03:27 +0530

    new code changes

--- summ.py
+++ summ.py
@@ -13,16 +13,18 @@ def sum2(url):
 
     if 'usatoday' in url:
         main_text = re.sub('\s+',' '," ".join([ x.text for x in soup.find_all('p') ][14:]))
-    if 'gizmodo' in url:
+    elif 'gizmodo' in url:
         main_text = re.sub('\s+',' ',soup.find_all('div','Normal')[0].text)    
-    if 'thehindu' in url:
+    elif 'thehindu' in url:
         articlelead = re.sub('\s+',' ',soup.find_all('div','articleLead')[0].text)
         main_text = re.sub('\s+',' '," ".join([ x.text for x in soup.find_all('p','body') ]))
-    if 'timesofindia' in url:
+    elif 'timesofindia' in url:
         if re.findall('<div class="Normal"',html):
             main_text = re.sub('\s+',' ',soup.find_all('div','Normal')[0].text)
         if re.findall('<div class="data"',html):
             main_text = re.sub('\s+',' ',soup.find_all('div','data')[0].text)
+    else:
+	main_text = re.sub('\s+',' '," ".join([ x.text for x in soup.find_all('p') ]))
     sents = nltk.sent_tokenize(main_text)
     words = map(nltk.word_tokenize,sents)
     title_words = map(string.lower,nltk.word_tokenize(soup.title.text))
@@ -33,5 +35,5 @@ def sum2(url):
     if len(summary)>5:
         summary = " ".join(summary[0:5])
     else: summary = " ".join(summary)
-    return u"Title: {0} <br/>Url: <a style='color:white;' target='_blank' href={1}>{1}</a><br/><br/>Summed Up:<br/>{2}<br><p style='float:right;color:white;font-size:11px;'>Report: <a style='color:rgb(86, 197, 51);' href='mailto:report@sumitraj.in?Subject={1}' target='_blank';>Help us Improve</a></p><p style='color:black;'>Placeholder</p>".format(soup.title.text,url, summary)
+    return u"Title: {0} <br/>Url: <a style='color:white;' target='_blank' href={1}>{1}</a><br/><br/>Summed Up:<br/>{2}<br><p style='float:right;color:white;font-size:11px;'>Report: <a style='color:white;' href='mailto:report@sumitraj.in?Subject={1}' target='_blank';>Help us Improve</a></p><p style='color:black;'>Placeholder</p>".format(soup.title.text,url, summary)
 
